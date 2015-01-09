7b8b08423c9e305218ff60fa3047d19e46a127cd
MJGM <MJGM@example.com>
2015-01-09 12:21:18 +0100

    Método listMatching añadido para listar solo determinador archivos que contengan una cadena

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -95,4 +95,20 @@ public class MusicOrganizer
             count++;
         }
     }
+    
+    /**
+     * 
+     */
+    public void listMaching(String searchString)
+    {
+        for (String filename : files)
+        {
+            if (filename.contains(searchString))
+            {
+                System.out.println(filename);
+            }
+            
+        }
+    }
+    
 }
